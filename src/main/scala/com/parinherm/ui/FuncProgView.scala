6519730b4d1cbff189870d8b6836d1193e88312d
Paul Martin <mussym@live.com>
2020-07-29 18:05:15 -0500

    adding functional programming exercises bit

--- src/main/scala/com/parinherm/ui/FuncProgView.scala
+++ src/main/scala/com/parinherm/ui/FuncProgView.scala
@@ -1,8 +1,9 @@
 package com.parinherm.ui
 
+import org.eclipse.jface.layout.GridDataFactory
 import org.eclipse.swt.SWT
 import org.eclipse.swt.custom.SashForm
-import org.eclipse.swt.layout.FillLayout
+import org.eclipse.swt.layout.{FillLayout, GridLayout}
 import org.eclipse.swt.widgets.{Composite, Label}
 
 object FuncProgView {
@@ -11,15 +12,41 @@ object FuncProgView {
     val root = new Composite(parent, SWT.NONE)
     val mainSash = new SashForm(root, SWT.HORIZONTAL)
     mainSash.setLayout(new FillLayout())
-    val listBox: Composite = new Composite(mainSash, SWT.NONE)
-    listBox.setLayout(new FillLayout())
-    val lblList = new Label(listBox, SWT.NONE).setText("List")
-    val dataBox: Composite = new Composite(mainSash, SWT.NONE)
-    dataBox.setLayout(new FillLayout())
-    val lblData = new Label(dataBox, SWT.NONE).setText("Data")
+
+    val listBox = createListBox(mainSash)
+    val dataBox = createDataBox(mainSash)
+
     mainSash.setWeights(Array(20, 80))
     root.setLayout(new FillLayout())
     root
+  }
+
+
+  def createListBox(parent: Composite) : Composite = {
+    val listBox: Composite = new Composite(parent, SWT.NONE)
+    listBox.setLayout(new FillLayout())
+    val lblList = new Label(listBox, SWT.NONE).setText("List")
+    listBox
+  }
+
+  def createDataBox(parent: Composite): Composite ={
+    /*
+    form that has:
+      label for displaying title
+      label for displaying notes
+      list of buttons for code
+      multiline label for code output
+    */
+    val dataBox: Composite = new Composite(parent, SWT.NONE)
+    dataBox.setLayout(new GridLayout(2, false))
+    val labelTitle = new Label(dataBox, SWT.NONE)
+    labelTitle.setText("Title")
+    GridDataFactory.swtDefaults().grab(true, false).applyTo(labelTitle)
+    val labelTitleContent = new Label(dataBox, SWT.NONE)
+    GridDataFactory.swtDefaults().grab(true, false).applyTo(labelTitleContent)
+    val labelHelpTitle = new Label(dataBox, SWT.NONE).setText("Help")
+    val labelHelpContent = new Label(dataBox, SWT.NONE).setText("Help Contents")
+    dataBox
   }
 
 }
