38649ec3f04c9d9858fed0a3e9bb10de9b0bb285
Paul Martin <mussym@live.com>
2020-08-03 19:17:15 -0500

    adding navigation model

--- src/main/scala/com/parinherm/model/CheatsheetNotes.scala
+++ src/main/scala/com/parinherm/model/CheatsheetNotes.scala
@@ -9,29 +9,28 @@ object CheatsheetNotes {
 
 
 
-  val firstExample =
-    """
-case classes - immutable fields, new not required
-return value object that represents a charge - as a case class
-don't actually charge the card
-pure functions allow for local reasoning
-eliminate side effects
+val variableAndMethodsHelp =
+"""
+laziness: use lazy keyword in front of variable declaration
+pattern matching: val (one, two) = ("one", 2)
+simple method:
+def add(n: Int, m: Int): Int = n + m
+curried method:
+def add(n: Int)(m: Int): Int = n + m
 
 """
 
-  val functionsHelp =
-    """
-      |um, functions
-      |""".stripMargin
-  val testFunction: () => String = ()  => "running a function"
-  val differentFunction: () => String = () => "running a diffferent function"
+  val demoVarsAndMethods: () => String = ()  => {
+    var output: String = ""
+    output += "Laziness lazy val z = 0"
+    output
+  }
 
   val exercises = ListBuffer.empty[Exercise]
-  exercises += new Exercise("Functions",  testFunction)
-  exercises += new Exercise("Currying", differentFunction)
+  exercises += new Exercise("Demo",  demoVarsAndMethods)
 
   val topics = ListBuffer.empty[Topic]
-  topics += new Topic("First Example", firstExample, exercises)
+  topics += new Topic("Variables And Methods", variableAndMethodsHelp, exercises)
 
 
 }
